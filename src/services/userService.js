bebe5ddca52bc9631e7eea97e17a5dddff1e2e5d
Damian Chamel <58256720+Damian-crypto@users.noreply.github.com>
2025-06-30 14:07:51 +0530

    Added error handling

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,10 +3,18 @@ import axios from "axios";
 const API_BASE_URL = "https://localhost:7172/api";
 
 const addUser = async (userData, token) => {
-  const response = await axios.post(`${API_BASE_URL}/Account/add-user`, userData, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-  return response.data;
+  try {
+    const response = await axios.post(`${API_BASE_URL}/Account/add-user`, userData, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      throw new Error(error.response.data[0] || "Server error");
+    } else {
+      throw new Error("Network error");
+    }
+  }
 };
 
 export { addUser };
